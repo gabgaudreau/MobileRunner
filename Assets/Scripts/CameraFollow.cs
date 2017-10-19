548a439824f816aa195c51c6a1b6ad9380308555
Gabriel Gaudreau <gabriel.gaudreau23@hotmail.com>
2017-10-18 20:05:28 -0400

    scenes, folders created, basic level, player going forward and infinite scrolling logic has been implemented

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿/**
+File Created October 18th 2017 - File name = CameraFollow.cs
+Author: Gabriel Gaudreau
+Project: MobileRunner
+*/
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    [SerializeField]
+    GameObject player;
+    private float yOffset = 5;
+    private float zOffset = -10;
+
+    /// <summary>
+    /// This update will make sure that the camera follows the player at a specific distance, constantly.
+    /// </summary>
+	void Update () {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + yOffset, player.transform.position.z + zOffset);
+	}
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,52 @@
+﻿/**
+File Created October 18th 2017 - File name = PlayerMove.cs
+Author: Gabriel Gaudreau
+Project: MobileRunner
+*/
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+    private const float PLAYER_MOVE_SPEED = 750.0f;
+    private const float DIFFICULTY_INCREASE = 0.015f;
+    private float difficulty;
+    private Rigidbody rb;
+    [SerializeField]
+    GameObject respawn;
+
+    /// <summary>
+    /// Start function, initiates some variables.
+    /// </summary>
+	void Start () {
+        difficulty = 1.0f;
+        rb = GetComponent<Rigidbody>();
+	}
+
+    /// <summary>
+    /// This will make the game wait for X amount of time. Then activates gravity, gives the ball a chance to land, bounce and land again before locking gravity.
+    /// </summary>
+    /// <returns>Waits X amount of time</returns>
+    IEnumerator ActivateGravity() {
+        yield return new WaitForSeconds(0.75f);
+        rb.useGravity = false;
+    } 
+
+    /// <summary>
+    /// OnTriggerEnter function, handles entering with multiple triggers, scaling difficulty as well as infinte scrolling logic is implemented in this function.
+    /// </summary>
+    /// <param name="col">Object that is the trigger</param>
+    void OnTriggerEnter(Collider col) {
+        if(col.gameObject.layer == LayerMask.NameToLayer("Pit")) {
+            transform.position = respawn.transform.position;
+            difficulty += DIFFICULTY_INCREASE;
+        }
+        else if(col.gameObject.layer == LayerMask.NameToLayer("GravityTriggerEnd")) {
+            rb.velocity = new Vector3(0, 0, 10);
+            rb.useGravity = true;
+        }
+        else if (col.gameObject.layer == LayerMask.NameToLayer("GravityTriggerStart")) {
+            rb.AddForce(Vector3.forward * PLAYER_MOVE_SPEED * difficulty);
+            StartCoroutine(ActivateGravity());
+        }
+    }
+}
